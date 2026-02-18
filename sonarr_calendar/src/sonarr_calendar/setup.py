5d4c2ceff4873c8d79832aa2a8b6167f4766355c
KBW1963 <kbewley@sky.com>
2026-02-18 19:15:01 +0000

    Add files via upload
    ??????

---
+++ sonarr_calendar/src/sonarr_calendar/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sonarr-calendar",
+    version="3.0.2",
+    author="Your Name",
+    description="Sonarr Calendar Tracker - HTML dashboard generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourname/sonarr-calendar",
+    package_dir={"": "src"},                # look for packages in src/
+    packages=find_packages(where="src"),    # find all packages under src/
+    include_package_data=True,
+    package_data={
+        "sonarr_calendar": ["templates/*.html"],   # include template files
+    },
+    install_requires=[
+        "requests>=2.28.0",
+        "jinja2>=3.1.0",
+        "colorama>=0.4.6",
+    ],
+    entry_points={
+        "console_scripts": [
+            "sonarr-calendar=sonarr_calendar.cli:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)

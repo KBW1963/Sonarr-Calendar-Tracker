a4fc7c212cc3e9637fcad7ea6b05436c72f38187
KBW1963 <kbewley@sky.com>
2026-02-22 18:23:28 +0000

    Delete sonarr_calendar/src directory
    Updating to a new structure

--- sonarr_calendar/src/sonarr_calendar/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Sonarr Calendar Tracker - A tool to generate a beautiful HTML dashboard of your Sonarr shows."""
-__version__ = "2.7.0 - fanart"
--- sonarr_calendar/src/sonarr_calendar/__main__.py
+++
@@ -1,5 +0,0 @@
-import sys
-from sonarr_calendar.cli import main
-
-if __name__ == "__main__":
-    sys.exit(main())
